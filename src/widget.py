0d362f5481c8fd8c6ead1186c350ec1c6927de1a
{rum} <rum@example.com>
2026-05-26 12:39:06 +0700

    added обработки account card into func mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -1,21 +1,23 @@
 import re
 
 
-def mask_account_card(text:str)->str:
-    card_number_mask = ''
+def mask_account_card(text_data:str)->str:
 
-    card_match = re.search(r'\b\d{16}\b', text)
+    card_match = re.search(r'\b\d{16}\b', text_data)
     if card_match:
         card_number = card_match.group()
         card_mask = card_number[0:6] + '*'*6 + card_number[-4:]
 
         # Добавляем пробелы после каждого 4‑го символа
-        card_number_mask = ' '.join(card_mask[i:i+4] for i in range(0, len(card_mask), 4))
+        return ' '.join(card_mask[i:i+4] for i in range(0, len(card_mask), 4))
 
-    else:
-        return f"Номер карты не обнаружен. Исходные данные: '{text}'"
-        return card_number_mask
-    return 'No data'
+    account_match = re.search(r'\b\d{20}\b', text_data)
+    if account_match:
+        account = account_match.group()
+        return "**" + account[-4:]
+
+    return "No data"
+
 
 if __name__ == '__main__':
     print(mask_account_card('Visa Platinum 7000792289606361'))
