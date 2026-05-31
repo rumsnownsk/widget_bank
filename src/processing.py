f300b67d5220e404724a8edee6a5c8fa30e5e951
{rum} <rum@example.com>
2026-05-31 14:53:11 +0700

    optimaze function filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,2 @@
 def filter_by_state(data:list, state:str = "EXECUTED") -> list:
-    new_list = []
-
-    for item in data:
-        if item['state'] == state:
-            new_list.append(item)
-
-    return new_list
+    return [item for item in data if item['state'] == state]
