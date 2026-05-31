760f846b9817dbd1930c4c195307d356e32e8e17
{rum} <rum@example.com>
2026-05-31 15:49:32 +0700

    added function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,2 +1,7 @@
 def filter_by_state(data:list, state:str = "EXECUTED") -> list:
+    """Функция возвращает список словарей, у которых state соответствует"""
     return [item for item in data if item['state'] == state]
+
+def sort_by_date(data:list, sort_by:str = "from_last") -> list:
+
+    return sorted(data, key= lambda x: x['date'], reverse=sort_by == 'from_last')
--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,7 @@
 import re
 
 from masks import get_mask_card_number, get_mask_account
-from processing import filter_by_state
+from processing import filter_by_state, sort_by_date
 
 
 def mask_account_card(text_data: str) -> str:
@@ -43,3 +43,13 @@ if __name__ == "__main__":
                            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
                            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
                           , 'CANCELED'))
+
+    print(sort_by_date(
+        [
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+        ],
+        'from'
+    ))
