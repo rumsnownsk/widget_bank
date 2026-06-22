0baec0e16494cf6a65146238723f082bdb363fba
{rum} <rum@example.com>
2026-06-22 16:41:26 +0700

    created tests for module processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,41 @@
+import re
+from datetime import datetime
+
 from typing import Literal
 
 
-def filter_by_state(data: list, state: str = "EXECUTED") -> list:
-    """Функция возвращает список словарей, у которых state соответствует"""
-    return [item for item in data if item["state"] == state]
+def filter_by_state(data: list, state: Literal["EXECUTED", "CANCELED"] = "EXECUTED") -> list:
+    """
+    Функция возвращает список словарей, у которых поле 'state' точно соответствует
+    указанному значению.
+
+    Args:
+        data (list): Список словарей с транзакциями.
+        state (str): Значение state для фильтрации. Допустимые значения:
+            - 'EXECUTED' (по умолчанию);
+            - 'CANCELED'
+
+    Returns:
+        list: Отфильтрованный список словарей.
+    """
+    if not isinstance(data, list):
+        raise TypeError("Parameter 'data' must be a list")
+
+    result = []
+    for item in data:
+        if not isinstance(item, dict):
+            continue
+        #проверка наличия ключа 'state' и его значение
+        item_state = item.get("state")
+        if isinstance(item_state, str) and item_state == state:
+            result.append(item)
+
+    return result
 
 
 def sort_by_date(data: list, sort_by: Literal["from_last", "from_first"] = "from_last") -> list:
     """
-    Функция сортирует данные по дате
+    Функция возвращает список словарей, отсортированных по полю 'date'.
     Args:
         data (list): Список словарей с полем 'date'.
         sort_by (str): Способ сортировки. Допустимые значения:
@@ -17,9 +44,31 @@ def sort_by_date(data: list, sort_by: Literal["from_last", "from_first"] = "from
 
     Returns:
         list: Отсортированный список словарей.
+
+    Raises:
+        TypeError: если data не является списком или sort_by имеет недопустимое значение
     """
-    if sort_by == "from_last":
-        return sorted(data, key=lambda x: x["date"], reverse=True)
-    elif sort_by == "from_first":
-        return sorted(data, key=lambda x: x["date"], reverse=False)
-    return data
+    if not isinstance(data, list):
+        raise TypeError("Parameter 'data' must be a list")
+
+    if sort_by not in ["from_last", "from_first"]:
+        raise ValueError("Parameter 'sort_by' must be 'from_last' or 'from_first'")
+
+    def parse_date(el: dict) -> datetime | None:
+        date_str = el.get("date")
+
+        if not isinstance(date_str, str):
+            return None
+        try:
+            return datetime.fromisoformat(date_str)
+        except ValueError:
+            return None
+
+    result = []
+    for item in data:
+        if isinstance(item, dict):
+            if parse_date(item) is not None:
+                result.append(item)
+    reverse = sort_by == "from_last"
+
+    return sorted(result, key=lambda x: x["date"], reverse=reverse)
---
+++ tests/test_processing.py
@@ -0,0 +1,58 @@
+import pytest
+
+from src.processing import filter_by_state, sort_by_date
+
+
+@pytest.fixture
+def coll():
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 0, "state": "", "date": ""},
+        {"id": 0, "state": None, "date": ""},
+        {"id": 0, "hello": "world", "str": "no"},
+        "not a dict",
+        {},
+    ]
+
+
+def test_filter_by_state_executed(coll):
+    assert filter_by_state(coll, "EXECUTED") == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+def test_filter_by_state_canceled(coll):
+    assert filter_by_state(coll, "CANCELED") == [
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+    ]
+
+
+def test_sort_by_date_from_first(coll):
+    assert sort_by_date(coll, "from_first") == [
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}
+    ]
+
+
+def test_sort_by_date_from_last(coll):
+    assert sort_by_date(coll, "from_last") == [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+    ]
+
+def test_invalid_sort_by():
+    with pytest.raises(ValueError):
+        sort_by_date([], "invalid_value")
+
+def test_non_list_input():
+    with pytest.raises(TypeError):
+        sort_by_date("not_a_list")
