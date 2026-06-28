d403bf5a9c33a2e99792264ff20dab56ddb0b07d
{rum} <rum@example.com>
2026-06-28 16:13:47 +0700

    ended homework

--- src/generators.py
+++ src/generators.py
@@ -1,33 +1,34 @@
 import json
-from pathlib import Path
 import random
+from pathlib import Path
 
 base_dir = Path(__file__).resolve().parent.parent
 file_path = base_dir / "data" / "transactions.json"
-
 
 transactions_json = []
 if not file_path.exists():
     print(f"Предупреждение: файл не найден: {file_path}. Возвращаем пустой набор транзакций.")
 else:
-    with open(file_path, 'r', encoding="utf-8") as f:
+    with open(file_path, "r", encoding="utf-8") as f:
         transactions_json = json.load(f)
 
-print('\n')
+print("\n")
 print("========= список транзакций по 'currency_code' (по умолчанию - USD) ======== ")
-def filter_by_currency(data, currency_code = "USD"):
+
+
+def filter_by_currency(data, currency_code="USD"):
     """
     Генератор для фильтрации транзакций по коду валюты.
     Фунция принимает итеррируемый объект и возвращает иттератор,
     который поочередно выдает транзакции,
     где валюта операции соответствует заданной
-    :param data: Итерируемый набор транзакций (обычно list[dict])
+    :param data: Итерируемый набор транзакций (list[dict])
     :param currency_code: Код валюты для фильтрации (по умолчанию "USD")
     :return: Генератор, выдающий подходящие транзакции
     """
     for t in data:
         try:
-            curr_code = t['operationAmount']['currency']['code']
+            curr_code = t["operationAmount"]["currency"]["code"]
         except KeyError:
             continue
         if curr_code == currency_code:
@@ -39,10 +40,17 @@ usd_transactions = filter_by_currency(transactions_json)
 for i in usd_transactions:
     print(i)
 
-print('\n')
+print("\n")
 print("========= возвращаем описание каждой операции по очереди ======== ")
 
+
 def transaction_descriptions(data):
+    """
+    Генератор для вывода описания транзакции.
+    Функция принимает итеррируемый объект и возвращает иттератор
+    :param data: Итерируемый набор транзакций (list[dict])
+    :return: Генератор строк — описаний транзакций
+    """
     for i in data:
         yield i["description"]
 
@@ -50,16 +58,22 @@ def transaction_descriptions(data):
 for i in transaction_descriptions(transactions_json):
     print(i)
 
-print('\n')
+print("\n")
 print("========= генерируем случайные номера банковских карт ======== ")
 
-def card_number_generator(count_card):
+
+def card_number_generator(start=0, end=9):
+    """
+    Генератор для вывода случайных номеров банковских карт.
+    """
+
     def four_digits():
-        digits = [str(random.randint(0, 9)) for _ in range(4)]
+        digits = [str(random.randint(start, end)) for _ in range(4)]
         return "".join(digits)
-    for _ in range(count_card):
+
+    for _ in range(5):
         yield f"{four_digits()} {four_digits()} {four_digits()} {four_digits()}"
 
-for number_card in card_number_generator(5):
-    print(number_card)
 
+for number_card in card_number_generator(1, 2):
+    print(number_card)
